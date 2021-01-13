dc6a3d7c6e0166cc675fa297f11dcc22dc22e48d
Jihad Banna <super_banna@hotmail.com>
2021-01-13 18:57:38 +0200

    ji2

---
+++ fetchByName.js
@@ -0,0 +1,40 @@
+function searchByName (){
+    
+    const searchName = document.querySelector('#search').value;
+    let showTitle = document.createElement("h2");
+    let showLang = document.createElement("h5");
+    let showDate = document.createElement("h5");
+    let showImg = document.createElement("img"); 
+    let showType = document.createElement("h5");
+
+    
+
+    fetch(`http://api.tvmaze.com/singlesearch/shows?q=${searchName}`)
+        .then(response => {
+            if (!response.ok) throw new Error(response.status);
+            return response.json();
+        })
+        .then(result => {
+
+            showTitle.textContent = result.name;
+            showImg.src = result.image.original;
+            showLang.textContent = result.language;
+            showDate.textContent = result.premiered;
+            showType.textContent = result.genrescd;
+
+            nameOutput.appendChild(showTitle);
+            nameOutput.appendChild(showImg);
+            nameOutput.appendChild(showLang);
+            nameOutput.appendChild(showDate);
+            nameOutput.appendChild(showType);
+        })
+        .catch(error => {
+            console.log(error);
+            if (error.message === "404") {
+                nameOutput.textContent = `⚠️ Couldn't find ${searchName}`;
+            } else {
+                nameOutput.textContent = "⚠️ Something went wrong";
+            }
+        });
+}
+
