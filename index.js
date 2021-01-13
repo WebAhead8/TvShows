4a2d85a9b5ee6fe3c6f3b215b0c0cb003075679e
mohamadsawaed <mohamad.sawaed.22@gmail.com>
2021-01-13 19:47:09 +0200

    added

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const searchForm = document.querySelector("form");
+const nameOutput = document.querySelector("output");
+const nameinput = document.querySelector("#inputtext");
+const radios = document.querySelectorAll("#radiosFieldSet input");
 
+let searchMethod;
+
+radios.forEach((input) => {
+  input.addEventListener("change", () => {
+    if (input.checked) {
+      if (input.value !== "date") {
+        document.getElementById("inputtext").setAttribute("type", "text");
+      } else {
+        document.getElementById("inputtext").setAttribute("type", "date");
+      }
+      searchMethod = input.value;
+    }
+  });
+});
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  nameOutput.innerHTML = "";
+  if (searchMethod === "date") {
+    // fetch data by data
+    fetchByDate(nameinput.value);
+  } else if (searchMethod === "country") {
+    fetchByCountry(nameinput.value);
+    // fetch by country
+  } else if (searchMethod === "name") {
+    fetchByName(nameinput.value);
+    // fetch by name
+  }
+
+  //const searchdate = document.querySelector("#date").value;
+});
